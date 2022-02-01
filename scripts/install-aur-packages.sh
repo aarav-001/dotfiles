32c07b1636e8927d241c37bffd7b92eae780d902
aarav-001 <98299827+aarav-001@users.noreply.github.com>
2022-02-01 20:14:04 +0530

    Update install-aur-packages.sh

--- scripts/install-aur-packages.sh
+++ scripts/install-aur-packages.sh
@@ -8,12 +8,8 @@ sudo chown -R $USER:$USER ./yay-git
 cd yay-git
 makepkg -si --no-confirm
 
-echo -e "\n======> Installing AUR Packages\n"
+echo -e "\n======> Installing AUR Packages...\n"
 
 yay -S $(<pkg/aur-packages.txt)
 
-echo -e "\n======> Installing oh-my-zsh\n"
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo -e "\n======> Completed Installing AUR Packages!\n"
